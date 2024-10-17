f07aa57f020835c93296432c44d55449f7391ae7
jkramer5103 <121427510+jkramer5103@users.noreply.github.com>
2024-10-17 19:53:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup, find_packages
+import secrets
 
 setup(
-    name='jlkutils',
-    version='0.15',
+    name=name,
+    version='0.1',
     packages=find_packages(),
-    install_requires=[
-        "cryptography", "opencv-python", "openai", "tkinter", "ast",
-    ],   author='Jaron Kramer',
-    description='Allgemeine Library für allen moglichen Praktischen Stuff',
-    url='https://github.com/jkramer5103/jlkutils',  # Optional: URL zu deinem Projekt
+    install_requires=packages,
+    description=description,
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        f'Operating System :: {opsy}',
     ],
     python_requires='>=3.6',
 )

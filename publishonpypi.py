5d4f9fc281ebb3fbab7f8c4e5bcf19e4387aa046
jkramer5103 <121427510+jkramer5103@users.noreply.github.com>
2024-10-17 11:05:34 +0200

    Add files via upload

---
+++ publishonpypi.py
@@ -0,0 +1,37 @@
+import os
+import jlkutils
+apikey = input("Gib deinen PyPi Api Key ein: ")
+author = input("Dein Voller Name (Wenn dieser offentlich sein soll): ")
+name = input("Wie soll das Package heißen? ")
+opsy = input("Welches Betriebssystem braucht dein Package? (Wenn Egal einfach Enter drücken): ")
+description = input("Beschreibe dein Package einfach: ")
+print("Wähle dein Package aus.")
+def listtostring(liste):
+    return ", ".join(liste)
+modulepath = jlkutils.choose_file()
+if opsy == "":
+    opsy = "OS Independent"
+packages_comma = input("Welche Packages Werden Benötigt? (Mit , Geteilt) "
+packageslist = str(packages_comma.split(","))
+modulepathlist = modulepath.split("\\")
+filename = modulepathlist[len(modulepathlist - 1)]
+filenamelist = filename.split(".")
+filenamenopy = filenamelist[len(filenamelist - 1)]
+os.system(f'mkdir {name}')
+os.chdir(name)
+jlkutils.writenewline("secrets.py", f"apikey = {apikey}")
+jlkutils.writenewline("secrets.py", f"packages = {packageslist}")
+jlkutils.writenewline("secrets.py", f"name = {name}")
+jlkutils.writenewline("secrets.py", f"author = {author}")
+jlkutils.writenewline("secrets.py", f"opsy = {opsy}")
+jlkutils.writenewline("secrets.py", f"description = {description}")
+os.system("curl -O https://raw.githubusercontent.com/jkramer5103/stuff/refs/heads/main/setup.py")
+os.system("curl -O https://raw.githubusercontent.com/jkramer5103/stuff/refs/heads/main/build.py")
+os.system(f"mkdir {name}")
+os.chdir(name)
+os.system(f'copy "{modulepath}" ')
+commands = jlkutils.getcoms(filename)
+liststring = listtostring(commands)
+jlkutils.writetofile("__init__.py", f"from .{filenamenopy} install {liststring}")
+os.system("cls")
+print("Um es zu Veröffentlichen, führe einfach nur build.py aus.")
